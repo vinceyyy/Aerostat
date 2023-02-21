46793346ae23df1fde60d4181caf44c343bae4c6
Vincent Yan <vincent.yan@blend360.com>
2023-02-21 09:11:04 -0500

    feat: implement proper excel model as aws lambda layer format

--- aerostat/aws/info.py
+++ aerostat/aws/info.py
@@ -1,5 +1,9 @@
 def info(event: dict, _) -> dict:
-    with open("/opt/index.html", "r") as f:
-        html = f.read()
-
-    return {"statusCode": 200, "body": html, "headers": {"Content-Type": "text/html"}}
+    if not event["queryStringParameters"]:
+        with open("/opt/index.html", "r") as f:
+            html = f.read()
+        return {
+            "statusCode": 200,
+            "body": html,
+            "headers": {"Content-Type": "text/html"},
+        }
